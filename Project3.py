8c3dff16b997b3897b21d95d99a6f74c9cc4efbb
Angela Long <angelalong124@icloud.com>
2026-04-25 15:43:24 -0400

    created temporary labels for clustering until clustering techniques is finsihed

--- Project3.py
+++ Project3.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 # Pre-processing Techniques
 
@@ -21,3 +24,17 @@ X = pd.get_dummies(X, drop_first=True)
 # Scaled data using StandardScaler to ensure that all features contribute equally to the clustering process
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+
+# Temporary labels for Clustering (waiting for actualy labels from clustering techniques):
+
+# K-Means labels
+kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
+kmeans_labels = kmeans.fit_predict(X_scaled)
+
+# Hierarchical labels
+hierarchical = AgglomerativeClustering(n_clusters=3, linkage="ward")
+hierarchical_labels = hierarchical.fit_predict(X_scaled)
+
+# DBSCAN labels
+dbscan = DBSCAN(eps=2, min_samples=10)
+dbscan_labels = dbscan.fit_predict(X_scaled)
